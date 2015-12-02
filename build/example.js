84c1b2c523a3855b4f3f8e3efc9e2ef14a858d18
damian <eldamiani@op.pl>
2015-12-02 18:01:38 +0100

    ograniczenie możliwości przeliczania do liczby 3999999

--- build/example.js
+++ build/example.js
@@ -21,12 +21,17 @@ var RomanCalc = React.createClass({
 		//		var tabRzymska = ["I","IV","V","IX","X","XL","L","XC","C","CD","D","CM","M","<span class='ovl'>IV</span>","<span class='ovl'>V</span>","<span class='ovl'>IX</span>","<span class='ovl'>X</span>","<span class='ovl'>XL</span>","<span class='ovl'>L</span>","<span class='ovl'>XC</span>","<span class='ovl'>C</span>","<span class='ovl'>CD</span>","<span class='ovl'>D</span>","<span class='ovl'>CM</span>","<span class='ovl'>M</span>","<span class='rlovl'>XL</span>","<span class='rlovl'>L</span>","<span class='rlovl'>XC</span>","<span class='rlovl'>C</span>","<span class='rlovl'>CD</span>","<span class='rlovl'>D</span>","<span class='rlovl'>CM</span>","<span class='rlovl'>M</span>"];
 		//		var tabArabska = [1,4,5,9,10,40,50,90,100,400,500,900,1000,4000,5000,9000,10000,40000,50000,90000,100000,400000,500000,900000,1000000,4000000,5000000,9000000,10000000,40000000,50000000,90000000,100000000];
 		var wynik = "";
-		var ile = value;
-		var i = this.valuesTab.length - 1;
-		for (i = this.valuesTab.length - 1; i >= 0; i--) {
-			while (ile >= this.valuesTab[i][0]) {
-				ile = ile - this.valuesTab[i][0];
-				wynik = wynik + this.valuesTab[i][1];
+
+		if (value > 3999999) {
+			wynik = "Za duża liczba";
+		} else {
+			var ile = value;
+			var i = this.valuesTab.length - 1;
+			for (i = this.valuesTab.length - 1; i >= 0; i--) {
+				while (ile >= this.valuesTab[i][0]) {
+					ile = ile - this.valuesTab[i][0];
+					wynik = wynik + this.valuesTab[i][1];
+				}
 			}
 		}
 		return React.createElement(
